b569cf5ce27ee2e3de8d7804ed657b0d92528e9b
Ikast <Ikastru@yandex.ru>
2020-04-27 13:51:39 +0300

    H9 V5

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -6,32 +6,40 @@ import java.util.Arrays;
 public class AbsInteger {
 
     public static AbsInteger add(AbsInteger num1, AbsInteger num2){
-
-       String str1 = " "+num1;
-       String str2 = " "+num2;
-       String strArr1[] = str1.split(" ");
-       String strArr2[] = str2.split(" ");
-       byte numArr1[] = new byte[strArr1.length];
-       byte numArr2[] = new byte[strArr2.length];
-       byte numArr[] = new byte[0];
-       if (strArr1.length == strArr2.length){
-           for (int i =strArr1.length-1; i>0; i-- ){
-               numArr[i] = (byte) (numArr1[i] + numArr2[i]);
-           }
-       } else if (strArr1.length > strArr2.length){
-           for (int i =strArr1.length-1; i>0; i-- ){
-               numArr[i] = (byte) (numArr1[i] + numArr2[i]);
-           }
-       } else if (strArr1.length < strArr2.length){
-           for (int i =strArr2.length-1; i>0; i-- ){
-               numArr[i] = (byte) (numArr1[i] + numArr2[i]);
-           }
-       }
-       String str = numArr.toString();
-       byte bn = Byte.parseByte(str);
-       ByteInteger byInt1 = new ByteInteger(bn);
-       return byInt1;
+        String str1 = ""+num1;
+        String str2 = ""+num2;
+        int c = Integer.parseInt(str1);
+        int d = Integer.parseInt(str2);
+        int q = c+d;
+        ByteInteger bint = new ByteInteger((byte)q);
+        return bint;
     }
+
+//       String str1 = " "+num1;
+//       String str2 = " "+num2;
+//       String strArr1[] = str1.split(" ");
+//       String strArr2[] = str2.split(" ");
+//       byte numArr1[] = new byte[strArr1.length];
+//       byte numArr2[] = new byte[strArr2.length];
+//       byte numArr[] = new byte[0];
+//       if (strArr1.length == strArr2.length){
+//           for (int i =strArr1.length-1; i>0; i-- ){
+//               numArr[i] = (byte) (numArr1[i] + numArr2[i]);
+//           }
+//       } else if (strArr1.length > strArr2.length){
+//           for (int i =strArr1.length-1; i>0; i-- ){
+//               numArr[i] = (byte) (numArr1[i] + numArr2[i]);
+//           }
+//       } else if (strArr1.length < strArr2.length){
+//           for (int i =strArr2.length-1; i>0; i-- ){
+//               numArr[i] = (byte) (numArr1[i] + numArr2[i]);
+//           }
+//       }
+//       String str = numArr.toString();
+//       byte bn = Byte.parseByte(str);
+//       ByteInteger byInt1 = new ByteInteger(bn);
+//       return byInt1;
+//    }
 
 }
 
