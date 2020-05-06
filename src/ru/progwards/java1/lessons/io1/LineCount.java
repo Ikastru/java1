4085a43fb1c2e5f8da8d8605a097dbb9275b19f1
Ikast <Ikastru@yandex.ru>
2020-05-06 12:51:26 +0300

    H10 V20

--- src/ru/progwards/java1/lessons/io1/LineCount.java
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -4,16 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class LineCount {
     public static int calcEmpty(String fileName) {
         int result = 0;
-        BufferedReader in = null;
         try {
-            in = new BufferedReader(new FileReader(fileName));
-            String line;
-            while ((line = in.readLine()) != null) {
-                if (line.equals("")) {
+            FileReader fileReader  = new FileReader(fileName);
+            Scanner scanner = new Scanner(fileReader);
+            while (scanner.hasNextLine()) {
+                String str = scanner.nextLine();
+                if (str.equals("")) {
                     result++;
                 }
             }
