7a2b3d392b760b244cb2fc3253ffe3c1b0e98036
Ikast <Ikastru@yandex.ru>
2020-05-22 14:24:10 +0300

    H14 V14

--- src/ru/progwards/java1/lessons/queues/Order.java
+++ src/ru/progwards/java1/lessons/queues/Order.java
@@ -23,37 +23,35 @@ public class Order {
 
     private double sum;
     private int num;
-    private static int count = 0;
+    private static int count = 1;
     //Новая переменная для сортировки в очереди. Класс (1,2,3) умножается на миллион и
     //прибавляется номер заказа. Так все Ордера в очереди будут отсортированы как нужно.
-    private int l = getC()*1000000 + num;
+    private long l;
+
 
     public Order(double sum){
         this.sum = sum;
-        this.num = count++;
-    }
-
-    public int getC(){
-        int c;
-        if (sum<=10000){
-            c=1;
-        } else if (sum<=20000){
-            c=2;
-        } else {
-            c=3;
-        }
-        return c;
+        this.num = ++count;
+        int c=0;
+        if (sum<=10000)
+            c = 1;
+        else if (sum<=20000)
+            c = 2;
+        else
+            c = 3;
+        this.l = c*100000 + num;
     }
 
     public double getSum() {
         return sum;
     }
 
-    public int getL() {
+    public long getL() {
         return l;
     }
 
     public int getNum() {
         return num;
     }
+
 }
--- src/ru/progwards/java1/lessons/queues/OrderQueue.java
+++ src/ru/progwards/java1/lessons/queues/OrderQueue.java
@@ -6,13 +6,13 @@ package ru.progwards.java1.lessons.queues;
  * 3 - заказы до 10000 руб включительно
  * 2 - заказы от 10000 до 20000 руб включительно
  * 1 - заказы от 20000 руб
- *
+ * <p>
  * 2.8 Создать метод, public Order get(), возвращающий первый заказ в очереди для
  * обслуживания. Вначале обслуживаются заказы класса 1, потом 2, потом 3.
  * Внутри каждого класса заказы должны обслуживаться в порядке поступления
  * (по номеру заказа). Метод не выбрасывает исключения, возвращает null в случае
  * пустой очереди.
- *
+ * <p>
  * Продумать, и, при необходимости, добавить в классы нужные методы и свойства,
  * для того, чтобы реализовать эту задачу.
  */
@@ -22,51 +22,107 @@ import java.util.PriorityQueue;
 
 public class OrderQueue {
 
-    Comparator<Order> comparator = new Comparator<>() {
+    static Comparator<Order> comparator = new Comparator<>() {
         @Override
         public int compare(Order o1, Order o2) {
-            return o1.getL() - o2.getL();
+            return (int) (o1.getL() - o2.getL());
         }
     };
 
-    PriorityQueue<Order> myQueue = new PriorityQueue<Order>(comparator);
+    static PriorityQueue<Order> myQueue = new PriorityQueue<Order>(comparator);
 
     /**
      * Или в компараторе нужно учитывать классы 1,2,3.
      *
      */
-
-//    public static Comparator<Order> numComparator = new Comparator<Order>(){
-//
-//        @Override
-//        public int compare(Order c1, Order c2) {
-//            int res = 0;
-//            if(c1.getSum()<=10000 && c2.getSum()>10000){
-//                res = 1;
-//            }
-//            if(c1.getSum()<=20000 && c2.getSum()>20000){
-//                res = 1;
-//            }
-//            if(c1.getSum()>10000 && c2.getSum()<=10000){
-//                res = -1;
-//            }
-//            if(c1.getSum()>20000 && c2.getSum()<=20000){
-//                res = -1;
-//            }
-//            if( (c1.getSum()<=10000 && c2.getSum()<=10000) || (c1.getSum()<=20000 && c2.getSum()<=20000)
-//            || (c1.getSum()>20000 && c2.getSum()>20000)) {
-//                res = (c1.getNum() - c2.getNum());
-//            }
-//            return res;
-//        }
-//    };
-
-
-    public void add(Order order){
+    
+    public static void add(Order order) {
         myQueue.add(order);
     }
 
-    public Order get(){
+    public static Order get() {
         return myQueue.poll();
+    }
+
+    public static String getStr() {
+        return " " + myQueue.poll().getL();
+    }
+
+    public static void main(String[] args) {
+        Order order1 = new Order(17041.0);
+        Order order2 = new Order(7265.0);
+        Order order3 = new Order(9844.0);
+        Order order4 = new Order(4366.0);
+        Order order5 = new Order(6975.0);
+        Order order6 = new Order(17385.0);
+        Order order7 = new Order(16082.0);
+        Order order8 = new Order(11516.0);
+        Order order9 = new Order(535.0);
+        Order order10 = new Order(19272.0);
+        Order order11 = new Order(7323.0);
+        Order order12 = new Order(13866.0);
+        Order order13 = new Order(28938.0);
+        Order order14 = new Order(23550.0);
+        Order order15 = new Order(12369.0);
+        Order order16 = new Order(7045.0);
+        Order order17 = new Order(24763.0);
+        Order order18 = new Order(29476.0);
+        Order order19 = new Order(24366.0);
+        Order order20 = new Order(4775.0);
+        Order order21 = new Order(6351.0);
+        Order order22 = new Order(3654.0);
+        Order order23 = new Order(24745.0);
+        add(order1);
+        add(order2);
+        add(order3);
+        add(order4);
+        add(order5);
+        add(order6);
+        add(order7);
+        add(order8);
+        add(order9);
+        add(order10);
+        add(order11);
+        add(order12);
+        add(order13);
+        add(order14);
+        add(order15);
+        add(order16);
+        add(order17);
+        add(order18);
+        add(order19);
+        add(order20);
+        add(order21);
+        add(order22);
+        add(order23);
+//        System.out.println(getStr());System.out.println(getStr());System.out.println(getStr());System.out.println(getStr());
+//        System.out.println(getStr());System.out.println(getStr());System.out.println(getStr());System.out.println(getStr());
+//        System.out.println(getStr());System.out.println(getStr());System.out.println(getStr());System.out.println(getStr());
+//        System.out.println(getStr());System.out.println(getStr());System.out.println(getStr());System.out.println(getStr());
+//        System.out.println(getStr());System.out.println(getStr());System.out.println(getStr());System.out.println(getStr());
+//        System.out.println(getStr());System.out.println(getStr());System.out.println(getStr());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
+        System.out.println(get());
     }
 }
